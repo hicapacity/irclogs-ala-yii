c4ad2d36f4f651125cc5cc88e089e751dedeab49
shu zOMG chen <shu.chen@freelancedreams.com>
2011-05-04 09:24:56 +0000

    Added some preliminary color highlighting in output

--- irclogger/protected/views/logger/index.php
+++ irclogger/protected/views/logger/index.php
@@ -2,9 +2,10 @@
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 <h2>View Logs For</h2>
-<ul>
+<ul class="log_list">
 <?php foreach($dates as $date){
-	$l = CHtml::link(CHtml::encode($date), array('logger/view', 'date'=>$date));
+	$d = DateTime::createFromFormat('Ymd', $date);
+	$l = CHtml::link(CHtml::encode($d->format('l, F j, Y')), array('logger/view', 'date'=>$date));
 	echo '<li><a href="">', $l, '</a></li>';
 }?>
 
--- irclogger/protected/views/logger/view.php
+++ irclogger/protected/views/logger/view.php
@@ -1,10 +1,17 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
 <h1><i><?php echo CHtml::encode(Yii::app()->name); ?></i> for <?php echo $date; ?></h1>
-<ul>
-<?php foreach ($lines as $line){
+<ul class="log_lines">
+<?php
+$last_speaker = null; 
+$speaker_list = array();
+foreach ($lines as $line){
 	$log_line = new LogLine($line);
-	echo '<li>';
+	if (!in_array($log_line->speaker, $speaker_list)){
+		$speaker_list[] = $log_line->speaker;
+	}
+	$pos = array_search($log_line->speaker, $speaker_list) % 5;
+	echo '<li class="speaker', $pos ,'">';
 	echo '<span class="time">', $log_line->time, '</span> ';
 	echo '<span class="speaker">', $log_line->speaker, '</span> ';
 	echo '<span class="text ', $log_line->type, '">', $log_line->text, '</span> ';
